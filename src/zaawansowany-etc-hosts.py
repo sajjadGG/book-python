b314fb5cdea715db764d47c0e356b9fb1e4073e4
Matt Harasymczuk <matt@astrotech.io>
2018-04-10 11:59:29 +0200

    Cleanup #time 20m

--- src/zaawansowany-etc-hosts.py
+++ src/zaawansowany-etc-hosts.py
@@ -3,11 +3,13 @@
 """ /etc/hosts
 ##
 # Host Database
+#   - Unix: /etc/hosts
+#   - Windows: C:/Windows/System32/drivers/etc/hosts
 ##
 
 127.0.0.1       localhost
-127.0.0.1       mycomp
-10.13.37.1      facebook.com google.com microsoft.com
+127.0.0.1       astromatt
+10.13.37.1      nasa.gov esa.int roscosmos.ru
 255.255.255.255 broadcasthost
 ::1             localhost
 """
@@ -17,7 +19,7 @@ hosts = []
 
 
 with open(FILENAME) as file:
-    for line in file:
+    for line in file.readlines():
         if not line.isspace() and not line.startswith('#'):
             ip, *hostnames = line.split()
 
@@ -31,10 +33,11 @@ from pprint import pprint
 pprint(hosts)
 
 """ Powinniśmy uzyskać efekt podobny do:
-
-{'ip': '127.0.0.1', 'hostnames': ['localhost'], 'protocol': 'ipv4'},
-{'ip': '127.0.0.1', 'hostnames': ['mycomp'], 'protocol': 'ipv4'},
-{'ip': '10.13.37.1', 'hostnames': ['facebook.com', 'google.com', 'microsoft.com'], 'protocol': 'ipv4'},
-{'ip': '255.255.255.255', 'hostnames': ['broadcasthost'], 'protocol': 'ipv4'},
-{'ip': '::1', 'hostnames': ['localhost'], 'protocol': 'ipv6'},
+[
+    {'ip': '127.0.0.1', 'hostnames': ['localhost'], 'protocol': 'ipv4'},
+    {'ip': '127.0.0.1', 'hostnames': ['astromatt'], 'protocol': 'ipv4'},
+    {'ip': '10.13.37.1', 'hostnames': ['nasa.gov', 'esa.int', 'roscosmos.ru'], 'protocol': 'ipv4'},
+    {'ip': '255.255.255.255', 'hostnames': ['broadcasthost'], 'protocol': 'ipv4'},
+    {'ip': '::1', 'hostnames': ['localhost'], 'protocol': 'ipv6'},
+]
 """
