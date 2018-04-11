4d5a77c7a260aac47c83e808b09fe89fba358ac6
Matt Harasymczuk <matt@astrotech.io>
2018-04-11 14:30:17 +0200

    Poprawki #time 10m

--- book/sredniozaawansowany/src/csv-passwd.py
+++ book/sredniozaawansowany/src/csv-passwd.py
@@ -1,13 +1,17 @@
 import csv
 
-FILENAME = r'C:\Users\Matt Harasymczuk\Desktop\passwd.txt'
+FILENAME = r'../../../data/file-etc-passwd.txt'
 
-with open(FILENAME) as csvfile:
+
+with open(FILENAME) as file:
     fieldnames = ['user', 'password', 'uid', 'gid', 'name', 'home', 'shell']
-    data = csv.DictReader(csvfile, fieldnames=fieldnames, delimiter=':')
+    data = csv.DictReader(file, fieldnames=fieldnames, delimiter=':')
 
     for row in data:
         print(row)
 
-# OrderedDict([('user', 'root'), ('password', 'x'), ('uid', '0'), ('gid', '0'), ('name', 'root'), ('home', '/root'), ('shell', '/bin/bash')])
-# OrderedDict([('user', 'bin'), ('password', 'x'), ('uid', '1'), ('gid', '1'), ('name', 'bin'), ('home', '/bin'), ('shell', '/sbin/nologin')])
+# OrderedDict([('user', '# User Database'), ('password', None), ('uid', None), ('gid', None), ('name', None), ('home', None), ('shell', None)])
+
+# OrderedDict([('user', 'jimenez'), ('password', 'x'), ('uid', '1001'), ('gid', '1001'), ('name', 'Jose Jimenez'), ('home', '/home/jimenez'), ('shell', '/bin/bash')])
+
+# OrderedDict([('user', 'ivanovic'), ('password', 'x'), ('uid', '1002'), ('gid', '1002'), ('name', 'Ivan Ivanovic'), ('home', '/home/ivanovic'), ('shell', '/bin/bash')])
