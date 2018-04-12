25314079598eb887ab84b809f07b20fe259c2b45
Matt Harasymczuk <matt@astrotech.io>
2018-04-12 12:01:20 +0200

    Generator #time 15m

--- book/zaawansowany/src/iterators-ksiazka-adresowa.py
+++ book/zaawansowany/src/iterators-ksiazka-adresowa.py
@@ -7,21 +7,20 @@ class Kontakt:
 
 class Adres:
     def __init__(self, **kwargs):
-        for key, value in kwargs:
+        for key, value in kwargs.items():
             setattr(self, key, value)
 
 
-ksiazka_adresowa = [
-    Kontakt(imie='Max', nazwisko='Peck', adresy=[
-        Adres(ulica='2101 E NASA Pkwy', miasto='Houston', stan='Texas',
-              kod='77058', panstwo='USA'),
-        Adres(ulica=None, miasto='Kennedy Space Center', kod='32899',
-              panstwo='USA'),
-        Adres(ulica='4800 Oak Grove Dr', miasto='Pasadena', kod='91109',
-              panstwo='USA'),
-        Adres(ulica='2825 E Ave P', miasto='Palmdale', stan='California',
-              kod='93550', panstwo='USA'),
-    ]),
-    Kontakt(imie='José', nazwisko='Jiménez'),
-    Kontakt(imie='Иван', nazwisko='Иванович', adresy=[]),
-]
+kontakt = Kontakt(imie='Max', nazwisko='Peck', adresy=[
+    Adres(ulica='2101 E NASA Pkwy', miasto='Houston', stan='Texas',
+          kod='77058', panstwo='USA'),
+    Adres(ulica=None, miasto='Kennedy Space Center', kod='32899',
+          panstwo='USA'),
+    Adres(ulica='4800 Oak Grove Dr', miasto='Pasadena', kod='91109',
+          panstwo='USA'),
+    Adres(ulica='2825 E Ave P', miasto='Palmdale', stan='California',
+          kod='93550', panstwo='USA'),
+])
+
+for adres in kontakt:
+    print(adres)
