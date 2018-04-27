889d7f5711bebfc91fa3a9f3ed1ee1022f228768
Matt Harasymczuk <matt@astrotech.io>
2018-04-27 09:22:29 +0200

    Poprawki #time 5m

--- src/podstawy-konwersja-liczby-na-slowny.py
+++ src/podstawy-konwersja-liczby-na-slowny.py
@@ -1,27 +1,22 @@
 #!/usr/bin/env python3
 
 LICZBY = {
-    1: 'jeden',
-    2: 'dwa',
-    3: 'trzy',
-    4: 'cztery',
-    5: 'pięć',
-    6: 'sześć',
-    7: 'siedem',
-    8: 'osiem',
-    9: 'dziewięć',
+    '1': 'jeden',
+    '2': 'dwa',
+    '3': 'trzy',
+    '4': 'cztery',
+    '5': 'pięć',
+    '6': 'sześć',
+    '7': 'siedem',
+    '8': 'osiem',
+    '9': 'dziewięć',
 }
 
-# ``input()`` zwraca stringa, symulujemy zachowanie funkcji uzywając ``str()``
-liczba_uzytkownika = str('911')
+# liczba_wprowadzona = input('Podaj liczbę: ')
+liczba_wprowadzona = str(911)  # ``input`` zawsze zwraca ``str``
 
-
-for cyfra in liczba_uzytkownika:
-    # indeksy w LICZBA są typu ``int``
-    # my iterujemy po ``str`` i cyfra jest typu ``str``
-    # rzutujemy cyfrę na ``int`` aby móc odnaleźć ją w LICZBY
-    index = int(cyfra)
-    print(LICZBY[index])
+for cyfra in liczba_wprowadzona:
+    print(LICZBY[cyfra])
 
 
 # Alternatywnie
@@ -36,13 +31,13 @@ def liczba_slownie(liczba: int) -> str:
     slownie = []
 
     for cyfra in str(liczba):
-        cyfra = int(cyfra)
         slownie.append(LICZBY[cyfra])
 
     return ' '.join(slownie)
 
 
 if __name__ == '__main__':
-    liczba_wprowadzona = input('Podaj liczbę: ')
+    # liczba_wprowadzona = input('Podaj liczbę: ')
+    liczba_wprowadzona = str(911)  # ``input`` zawsze zwraca ``str``
     napis = liczba_slownie(liczba_wprowadzona)
     print(napis)
