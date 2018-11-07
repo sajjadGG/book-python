48426a6662222d1e81f149509347e8c9195e30ea
Matt Harasymczuk <matt@astrotech.io>
2018-11-07 09:30:28 +0100

    HTTP: Scrapping #time 30m

--- http/src/scrapping_eva.py
+++ http/src/scrapping_eva.py
@@ -1,5 +1,6 @@
 import csv
 from datetime import datetime
+from pprint import pprint
 from bs4 import BeautifulSoup
 import requests
 
@@ -13,9 +14,9 @@ FILENAME_CSV = 'eva.csv'
 # It saves bandwidth, and speeds you development
 # Then comment following ``with`` context manager
 
-with open(FILENAME_HTML, mode='w', encoding='utf-8') as file:
-    response = requests.get(URL)
-    file.write(response.text)
+# with open(FILENAME_HTML, mode='w', encoding='utf-8') as file:
+#     response = requests.get(URL)
+#     file.write(response.text)
 
 
 # Parser content below
@@ -27,14 +28,46 @@ html = BeautifulSoup(content, 'html.parser')
 table = html.find_all('table')[1]
 
 
-def parse_date(text):
+def clean_Date(text):
     try:
-        return datetime.strptime(text, '%d %B %Y')
+        return datetime.strptime(text, '%d %B %Y').date()
     except ValueError:
         return text
 
 
-def parse_str(text):
+def clean_Number(text):
+    return int(text)
+
+
+def clean_ISS(text):
+    if text == '\xa0':
+        return ''
+    return text
+
+
+def clean_Mission(text):
+    return text
+
+
+def clean_Participants(text):
+    return ', '.join(x.string for x in text)
+
+
+def clean_Duration(text):
+    hours, minutes = text.split(':')
+    output = ''
+
+    if int(hours) != 0:
+        output += f'{hours}h '
+
+    if int(minutes[0]) == 0:
+        minutes = minutes[1]
+
+    output += f'{minutes}m'
+    return output
+
+
+def clean_Notes(text):
     if text == '\xa0':
         return ''
     return text
@@ -44,16 +77,15 @@ records = []
 
 for row in table.find_all('tr')[1:]:
     td = row.find_all('td')
-    participants = ','.join(x.string for x in td[3].find_all('a'))
 
     records.append({
-        'Number': parse_str(td[0].string),
-        'ISS': parse_str(td[1].string),
-        'Mission': parse_str(td[2].string),
-        'Participants': participants,
-        'Duration': parse_str(td[4].string),
-        'Date': parse_date(td[5].string),
-        'Notes': parse_str(td[6].string),
+        'Number': clean_Number(td[0].string),
+        'ISS': clean_ISS(td[1].string),
+        'Mission': clean_Mission(td[2].string),
+        'Participants': clean_Participants(td[3].find_all('a')),
+        'Duration': clean_Duration(td[4].string),
+        'Date': clean_Date(td[5].string),
+        'Notes': clean_Notes(td[6].string),
     })
 
 
@@ -70,3 +102,7 @@ with open(FILENAME_CSV, mode='w', encoding='utf-8') as file:
 
     for row in records:
         writer.writerow(row)
+
+
+pprint(records)
+
