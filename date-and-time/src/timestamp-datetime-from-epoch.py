b6d9677c7905409eeff94d2377f0ad7f9200befb
Matt Harasymczuk <matt@astrotech.io>
2018-12-06 18:36:59 +0100

    Foundations: Datetime #time 1m

--- date-and-time/src/timestamp-datetime-from-epoch.py
+++ date-and-time/src/timestamp-datetime-from-epoch.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 
 
-# Tuesday, June 27, 1978 17:27:00 GMT+02:00
 hermaszewski_timestamp = 267809220
+# Tuesday, June 27, 1978 17:27:00 GMT+02:00
 
 datetime.fromtimestamp(hermaszewski_timestamp)
 # datetime.datetime(1978, 6, 27, 15, 27)
--- date-and-time/src/timestamp-datetime-from-js.py
+++ date-and-time/src/timestamp-datetime-from-js.py
@@ -1,9 +1,11 @@
 from datetime import datetime
 
 
-# JavaScript has timestamp in miliseconds you have to divide by 1e3 = 1000
 timestamp = 267809220000
+# JavaScript has timestamp in miliseconds
+
 MILISECONDS = 1e3
+# To convert from miliseconds we have to divide by 1e3 = 1000
 
 datetime.fromtimestamp(timestamp / MILISECONDS)
 # datetime.datetime(1978, 6, 27, 17, 27)
