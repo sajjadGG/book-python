a132c629c11678d71e9443a06a9b082fb438d730
Matt Harasymczuk <matt@astrotech.io>
2019-05-23 11:19:52 +0200

    Network: Scrapping #time 45m

---
+++ http/solution/scrapping_iris.py
@@ -0,0 +1,34 @@
+from pprint import pprint
+from bs4 import BeautifulSoup
+import requests
+
+
+URL = 'https://github.com/AstroMatt/book-python/blob/master/numerical-analysis/data/iris-dirty.csv'
+HEADER = ['Sepal length', 'Sepal width', 'Petal length', 'Petal width', 'Species']
+OUTPUT = list()
+
+
+with open('/tmp/iris-dirty.html', mode='rw') as file:
+    response = requests.get(URL)
+    file.write(response.text)
+
+with open('/tmp/iris-dirty.html', mode='r') as file:
+    html = BeautifulSoup(file, 'lxml')
+
+
+table = html.find_all('table')[0]
+
+table_rows = table.find_all('tr')
+table_header = table_rows[0]
+table_body = table_rows[1:]
+
+species = table_header.text.split()[2:]
+
+for cell in table_body:
+    values = cell.text.split()
+    out = dict(zip(HEADER, values))
+    species_id = int(out['Species'])
+    out['Species'] = species[species_id]
+    OUTPUT.append(out)
+
+pprint(OUTPUT)
