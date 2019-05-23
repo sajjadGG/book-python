9c97f9f93b5ebed9c55baccbde6de2e5894bd538
Matt Harasymczuk <matt@astrotech.io>
2019-05-23 10:27:32 +0200

    Network: POP3 #time 60m

--- network/src/pop3-retrieve.py
+++ network/src/pop3-retrieve.py
@@ -1,20 +1,18 @@
-import getpass
 import poplib
 
 
-USERNAME = getpass.getuser()
-PASSWORD = getpass.getpass()
 HOSTNAME = 'localhost'
 PORT = poplib.POP3_SSL_PORT
+USERNAME = 'myusername'
+PASSWORD = 'mypassword'
 
 
 server = poplib.POP3_SSL(host=HOSTNAME, port=PORT, timeout=30)
 server.user(USERNAME)
 server.pass_(PASSWORD)
 
-
-numMessages = len(server.list()[1])
-
-for i in range(numMessages):
-    for j in server.retr(i + 1)[1]:
-        print(j)
+for message in server.list()[1]:
+    msgid, length = message.split()
+    status, content, length = server.retr(int(msgid))
+    content = list(line.decode() for line in content)
+    print(content)
