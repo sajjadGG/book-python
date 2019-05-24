3dc6f32f0c88d216076536deb548c3f8a5271b98
Matt Harasymczuk <matt@astrotech.io>
2019-05-24 10:19:55 +0200

    Network: Socket #time 1m

--- network/src/socket-tcp-client.py
+++ network/src/socket-tcp-client.py
@@ -3,7 +3,6 @@ import socket
 
 SERVER_HOST = '127.0.0.1'
 SERVER_PORT = 1337
-PAYLOAD = 'Hello World'
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
@@ -11,8 +10,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     print(f'Connecting to {SERVER_HOST}:{SERVER_PORT}/TCP')
     sock.connect((SERVER_HOST, SERVER_PORT))
 
-    print(f'Sending: "{PAYLOAD}"')
-    sock.sendall(PAYLOAD.encode())
+    payload = 'Hello World'
+    sock.sendall(payload.encode())
 
     data = sock.recv(1024).decode()
     print(f'Received: "{data}"')
