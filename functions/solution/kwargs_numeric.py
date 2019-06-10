b09d6a3a14ae76febde83330d9f3d0f3be692a71
Matt Harasymczuk <matt@astrotech.io>
2019-06-10 14:55:49 +0200

    Functions: Kwargs #time 1m

--- functions/solution/kwargs_numeric.py
+++ functions/solution/kwargs_numeric.py
@@ -1,13 +1,22 @@
 def is_numeric(*args):
+    if not args:
+        return False
+
     for arg in args:
         if not isinstance(arg, (int, float)):
             return False
+
     return True
 
 
 def is_numeric(*args, **kwargs):
     data = list(args) + list(kwargs.values())
+
+    if not data:
+        return False
+
     for arg in data:
         if not isinstance(arg, (int, float)):
             return False
+
     return True
