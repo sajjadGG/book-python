29a0b8880a759b30e5d442754b9cd2b9bffa951e
Matt Harasymczuk <matt@astrotech.io>
2019-06-27 11:26:53 +0200

    Foundations: Serialization #time 4m

---
+++ serialization/solution/csv_dictwriter_fixed.py
@@ -0,0 +1,25 @@
+import csv
+
+
+DATA = [
+    {'first_name': 'Jan',  'last_name': 'Twardowski'},
+    {'first_name': 'José', 'last_name': 'Jiménez'},
+    {'first_name': 'Mark', 'last_name': 'Watney'},
+    {'first_name': 'Иван', 'last_name': 'Иванович'},
+    {'first_name': 'Alex', 'last_name': 'Vogel'},
+]
+
+
+with open(r'../tmp/iris.csv', mode='w') as file:
+    writer = csv.DictWriter(
+        f=file,
+        fieldnames=['first_name', 'last_name'],
+        delimiter=',',
+        quotechar='"',
+        quoting=csv.QUOTE_ALL,
+        lineterminator='\n')
+
+    writer.writeheader()
+
+    for row in DATA:
+        writer.writerow(row)
