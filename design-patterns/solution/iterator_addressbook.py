3369d8d0c4bdecccca00ead8c00c32e5256c806a
Matt Harasymczuk <matt@astrotech.io>
2019-07-04 11:41:37 +0200

    Advanced: Design Patterns #time 2m

--- design-patterns/solution/iterator_addressbook.py
+++ design-patterns/solution/iterator_addressbook.py
@@ -27,14 +27,10 @@ class Adres:
 
 
 kontakt = Kontakt(imie='Jan', nazwisko='Twardowski', adresy=[
-    Adres(ulica='2101 E NASA Pkwy', miasto='Houston', stan='Texas',
-          kod='77058', panstwo='USA'),
-    Adres(ulica=None, miasto='Kennedy Space Center', kod='32899',
-          panstwo='USA'),
-    Adres(ulica='4800 Oak Grove Dr', miasto='Pasadena', kod='91109',
-          panstwo='USA'),
-    Adres(ulica='2825 E Ave P', miasto='Palmdale', stan='California',
-          kod='93550', panstwo='USA'),
+    Adres(ulica='2101 E NASA Pkwy', miasto='Houston', stan='Texas', kod='77058', panstwo='USA'),
+    Adres(ulica=None, miasto='Kennedy Space Center', kod='32899', panstwo='USA'),
+    Adres(ulica='4800 Oak Grove Dr', miasto='Pasadena', kod='91109', panstwo='USA'),
+    Adres(ulica='2825 E Ave P', miasto='Palmdale', stan='California', kod='93550', panstwo='USA'),
 ])
 
 for adres in kontakt:
--- design-patterns/src/iterators-ksiazka-adresowa.py
+++ design-patterns/src/iterators-ksiazka-adresowa.py
@@ -1,5 +1,5 @@
 class Kontakt:
-    def __init__(self, imie, nazwisko, adresy=[]):
+    def __init__(self, imie, nazwisko, adresy=()):
         self.imie = imie
         self.nazwisko = nazwisko
         self.adresy = adresy
@@ -12,14 +12,10 @@ class Adres:
 
 
 kontakt = Kontakt(imie='Jan', nazwisko='Twardowski', adresy=[
-    Adres(ulica='2101 E NASA Pkwy', miasto='Houston', stan='Texas',
-          kod='77058', panstwo='USA'),
-    Adres(ulica=None, miasto='Kennedy Space Center', kod='32899',
-          panstwo='USA'),
-    Adres(ulica='4800 Oak Grove Dr', miasto='Pasadena', kod='91109',
-          panstwo='USA'),
-    Adres(ulica='2825 E Ave P', miasto='Palmdale', stan='California',
-          kod='93550', panstwo='USA'),
+    Adres(ulica='2101 E NASA Pkwy', miasto='Houston', stan='Texas', kod='77058', panstwo='USA'),
+    Adres(ulica=None, miasto='Kennedy Space Center', kod='32899', panstwo='USA'),
+    Adres(ulica='4800 Oak Grove Dr', miasto='Pasadena', kod='91109', panstwo='USA'),
+    Adres(ulica='2825 E Ave P', miasto='Palmdale', stan='California', kod='93550', panstwo='USA'),
 ])
 
 for adres in kontakt:
