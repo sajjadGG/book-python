f5d02b5e9f4ed1861011a0d15ad37e3da399e6a7
Matt Harasymczuk <matt@astrotech.io>
2019-07-04 12:09:34 +0200

    Advanced: Design Patterns #time 5m

--- design-patterns/solution/iterator_addressbook.py
+++ design-patterns/solution/iterator_addressbook.py
@@ -1,18 +1,21 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class Kontakt:
-    def __init__(self, imie, nazwisko, adresy=[]):
-        self.imie = imie
-        self.nazwisko = nazwisko
-        self.adresy = adresy
+    first_name: str
+    last_name: str
+    addresses: tuple = ()
 
     def __iter__(self):
         self.current_element = 0
         return self
 
     def __next__(self):
-        if self.current_element >= len(self.adresy):
+        if self.current_element >= len(self.addresses):
             raise StopIteration
 
-        address = self.adresy[self.current_element]
+        address = self.addresses[self.current_element]
         self.current_element += 1
         return address
 
@@ -26,12 +29,12 @@ class Adres:
         return f'{self.__dict__}'
 
 
-kontakt = Kontakt(imie='Jan', nazwisko='Twardowski', adresy=[
+kontakt = Kontakt(first_name='Jan', last_name='Twardowski', addresses=(
     Adres(ulica='2101 E NASA Pkwy', miasto='Houston', stan='Texas', kod='77058', panstwo='USA'),
     Adres(ulica=None, miasto='Kennedy Space Center', kod='32899', panstwo='USA'),
     Adres(ulica='4800 Oak Grove Dr', miasto='Pasadena', kod='91109', panstwo='USA'),
     Adres(ulica='2825 E Ave P', miasto='Palmdale', stan='California', kod='93550', panstwo='USA'),
-])
+))
 
 for adres in kontakt:
     print(adres)
