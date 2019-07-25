f10b90030a4f2b3e22c71cb45a41cfe0e6cccc23
Matt Harasymczuk <matt@astrotech.io>
2019-07-25 16:17:21 +0200

    Foundations: OOP #time 2m

--- object-oriented-programming/solution/introduction_iris.py
+++ object-oriented-programming/solution/introduction_iris.py
@@ -1,25 +1,27 @@
 class Iris:
     def __init__(self, sepal_length: float, sepal_width: float, petal_length: float, petal_width: float, species: str):
-        self.sepal_length = float(sepal_length)
-        self.sepal_width = float(sepal_width)
-        self.petal_length = float(petal_length)
-        self.petal_width = float(petal_width)
-        self.species = str(species)
+        self.sepal_length: float = float(sepal_length)
+        self.sepal_width: float = float(sepal_width)
+        self.petal_length: float = float(petal_length)
+        self.petal_width: float = float(petal_width)
+        self.species: str = str(species)
 
-    def total(self):
+    def total(self) -> float:
         return self.sepal_length \
                + self.sepal_width \
                + self.petal_length \
                + self.petal_width
 
-    def average(self):
+    def average(self) -> float:
         return self.total() / 4
 
 
 setosa = Iris(5.4, 3.9, 1.3, 0.4, 'setosa')
+print(f'{setosa.species}')
 print(f'Total: {setosa.total():.2f}')
 print(f'Average: {setosa.average():.2f}')
 
 virginica = Iris(5.8, 2.7, 5.1, 1.9, 'virginica')
+print(f'{virginica.species}')
 print(f'Total: {virginica.total():.2f}')
 print(f'Average: {virginica.average():.2f}')
