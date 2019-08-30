774ddf5434f1ec44d3dbaf67c937c61d8be2d9f3
Matt Harasymczuk <matt@astrotech.io>
2019-08-30 12:19:49 +0200

    Intermediate: Optimization #time 5m

--- performance/solution/optimization_memoize.py
+++ performance/solution/optimization_memoize.py
@@ -1,39 +1,26 @@
-from timeit import timeit
 CACHE = {}
+
+
+def factorial_cache(n: int) -> int:
+    if n in CACHE:
+        return CACHE[n]
+
+    if n == 0:
+        return 1
+    else:
+        result = CACHE[n] = n * factorial_cache(n-1)
+        return result
+
+
+# Do not modify anything below
+from timeit import timeit
 
 
 def factorial_nocache(n: int) -> int:
     if n == 0:
         return 1
     else:
-        result = n * factorial_nocache(n-1)
-        return result
-
-
-def factorial_cache(n: int) -> int:
-    result = CACHE.get(n)
-
-    if result:
-        return result
-
-    if n == 0:
-        return 1
-    else:
-        result = n * factorial_cache(n-1)
-        CACHE[n] = result
-        return result
-
-
-# Alternative implementation
-def factorial_cache(n):
-    result = CACHE.get(n)
-
-    if result:
-        return result
-    else:
-        result = factorial_nocache(n)
-        CACHE[n] = result
-        return result
+        return n * factorial_nocache(n - 1)
 
 
 duration_cache = timeit(
@@ -51,4 +38,3 @@ duration_nocache = timeit(
 print(f'factorial_cache time: {round(duration_cache, 4)} seconds')
 print(f'factorial_nocache time: {round(duration_nocache, 3)} seconds')
 print(f'Cached solution is {round(duration_nocache / duration_cache, 1)} times faster')
-
