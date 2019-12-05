380edde75bc5ee757f2bf7912fe533da1ec22398
Matt Harasymczuk <matt@astrotech.io>
2019-12-05 13:48:30 +0100

    Numerical: Numpy #time 10m

--- numerical-analysis/numpy/solution/numpy_concatenation.py
+++ numerical-analysis/numpy/solution/numpy_concatenation.py
@@ -2,9 +2,8 @@ import numpy as np
 
 a = np.array([1, 2, 3])
 b = np.array([4, 5, 6])
-c = np.concatenate((a, b))
-output = c.reshape(2, 3)
+c = np.concatenate((a, b)).reshape(2, 3)
 
-print(output)
+print(c)
 # [[1 2 3]
 #  [4 5 6]]
