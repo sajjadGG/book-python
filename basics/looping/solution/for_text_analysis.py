3863a7d17165506aad8c87b0cc41dca7f77ad36e
Matt Harasymczuk <matt@astrotech.io>
2020-02-21 21:40:16 +0100

    Basic: Looping #time 15m

--- basics/looping/solution/for_text_analysis.py
+++ basics/looping/solution/for_text_analysis.py
@@ -40,3 +40,26 @@ print(f'Characters: {total_chars}')
 print(f'Letters: {total_letters}')
 print(f'Commas: {total_commas}')
 print(f'Adverbs: {total_adverbs}')
+
+
+
+# Alternative Solution
+print('-' * 50)
+TEXT = 'We choose to go to the Moon. We choose to go to the Moon in this decade and do the other things. Not because they are easy, but because they are hard. Because that goal will serve to organize and measure the best of our energies and skills. Because that challenge is one that we are willing to accept. One we are unwilling to postpone. And one we intend to win'
+text = TEXT.strip()
+
+OUTPUT = {
+    'Sentences': len(text.split('.')),
+    'Words': len(text.split(' ')),
+    'Characters': len(text.replace('. ', '')),
+    'Letters': len(text.replace('.', '').replace(' ', '').replace(',', '')),
+    'Commas': text.count(','),
+    'Adverbs': text.count('ly'),
+}
+
+for key, value in OUTPUT.items():
+    print(f'{key}: {value}')
+
+
+
+
