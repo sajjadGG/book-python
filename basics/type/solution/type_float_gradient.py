a9ab1379abe41ee90d481f8ec1597932b6fbdcfa
Matt Harasymczuk <matt@astrotech.io>
2020-07-04 13:59:52 +0200

    Basic: Float #time 1m

--- basics/type/solution/type_float_gradient.py
+++ basics/type/solution/type_float_gradient.py
@@ -4,7 +4,7 @@ hPa = 100 * Pa
 ATA = 1013.25 * hPa
 
 o2 = 20.946/100 * ATA
-pressure_gradient = 11.3 * Pa / M
+pressure_gradient = 11.3 * Pa / m
 death_altitude = (ATA - o2) / pressure_gradient
 
 print(f'Death altitude: {death_altitude:.2f} m')
