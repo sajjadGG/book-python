f34cce457c92270ee418f7eb3f8db3f92568a9b7
Matt Harasymczuk <matt@astrotech.io>
2020-07-04 14:35:57 +0200

    Basic: Bool #time 5m

--- basics/type/solution/type_bool_true_or_false.py
+++ basics/type/solution/type_bool_true_or_false.py
@@ -1,17 +1,17 @@
 a = bool(False)
 b = bool(True)
 
-c = bool('a')
-d = bool('.')
-e = bool('0')
-f = bool('0.0')
-g = bool('')
-h = bool(' ')
+c = bool(0)
+d = bool(0.0)
+e = bool(-0)
+f = bool(-0.0)
 
-i = bool(0)
-j = bool(0.0)
-k = bool(-0)
-l = bool(-0.0)
+g = bool('a')
+h = bool('.')
+i = bool('0')
+j = bool('0.0')
+k = bool('')
+l = bool(' ')
 
 m = bool(int('0'))
 n = bool(bool(float(str(-0))))
@@ -22,16 +22,16 @@ p = bool('-0.0+0.0j')
 
 print(a)    # False
 print(b)    # True
-print(c)    # True
-print(d)    # True
-print(e)    # True
-print(f)    # True
-print(g)    # False
+print(c)    # False
+print(d)    # False
+print(e)    # False
+print(f)    # False
+print(g)    # True
 print(h)    # True
-print(i)    # False
-print(j)    # False
+print(i)    # True
+print(j)    # True
 print(k)    # False
-print(l)    # False
+print(l)    # True
 print(m)    # False
 print(n)    # False
 print(o)    # False
