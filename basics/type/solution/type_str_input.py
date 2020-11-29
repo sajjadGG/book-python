795a13a061dbc33419199e47e1039e6db2e3bfac
Matt Harasymczuk <matt@astrotech.io>
2020-11-29 19:07:42 +0100

    Basic: Types #time 10m

--- basics/type/solution/type_str_input.py
+++ basics/type/solution/type_str_input.py
@@ -2,22 +2,24 @@
 * Assignment: Type String Input
 * Filename: type_str_input.py
 * Complexity: easy
-* Lines of code to write: 2 lines
-* Estimated time: 3 min
+* Lines of code to write: 1 lines
+* Estimated time: 2 min
 
 English:
-    1. Ask user to input text
-    2. Define `result` with number of characters of input text
+    1. Ask user to input text `NASA` (case sensitive)
+    2. Define `result` with text from user
 
 Polish:
-    1. Poproś użytkownika o wprowadzenie tekstu
-    2. Zdefiniuj `result` z liczbą znaków wprowadzonego tekstu
+    1. Poproś użytkownika o wprowadzenie tekstu `NASA` (wielkość liter ma znaczenie)
+    2. Zdefiniuj `result` z tekstem wprowadzonym od użytkownika
 
 Tests:
     >>> type(result)
-    <class 'int'>
+    <class 'str'>
+    >>> result
+    'NASA'
 """
 
+
 # Solution
-text = input('Type text: ')
-result = len(text)
+result = input('Type text: ')
