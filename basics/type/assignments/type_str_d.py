7b2e4466f3722ce2ac391ab6668a695b95a2191b
Matt Harasymczuk <matt@astrotech.io>
2021-03-17 09:30:32 +0100

    Basics: Types #time 30m

--- basics/type/assignments/type_str_d.py
+++ basics/type/assignments/type_str_d.py
@@ -26,3 +26,31 @@ DATA = 'UL. jana \tTWArdoWskIEGO 3'
 
 # Solution
 result = DATA.replace('UL. ', '').replace('\t', '').title().replace('3', 'III')
+
+
+# Alternative Solution
+result = (DATA
+        # Convert to common format
+        .upper()
+        # Remove unwanted whitespaces
+        .replace('\n', '')
+        .replace('\t', '')
+        .replace('     ', '')
+        .replace('    ', '')
+        .replace('   ', '')
+        .replace('  ', '')
+        # Remove unwanted special characters
+        .replace('.', '')
+        .replace(',', '')
+        .replace('-', '')
+        .replace('|', '')
+        # Remove unwanted text
+        .replace('ULICA', '')
+        .replace('UL', '')
+        .replace('TRZECIEGO', 'III')
+        .replace('3', 'III')
+        # Formatting
+        .title()
+        .replace('Iii', 'III')
+        .replace('Ii', 'II')
+        .strip())
