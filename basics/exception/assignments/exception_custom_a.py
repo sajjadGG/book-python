41d496157152c8eabd7db5cf7ab7a63390599918
Matt Harasymczuk <matt@astrotech.io>
2021-03-31 12:49:23 +0200

    Basic: Exception #time 2m

--- basics/exception/assignments/exception_custom_a.py
+++ basics/exception/assignments/exception_custom_a.py
@@ -6,14 +6,21 @@
 
 English:
     1. Ask user to input angle in degrees
-    2. Validate value passed to a `check` function
     2. Define custom exception `NegativeKelvin`
     3. If input temperature is lower than 0, raise `NegativeKelvin`
+    4. Non-functional requirements
+        a. Write solution inside `check` function
+        b. Mind the indentation level
+    5. Compare result with "Tests" section (see below)
 
 Polish:
     1. Poproś użytkownika o wprowadzenie kąta
     2. Zdefiniuj własny wyjątek `NegativeKelvin`
     3. Jeżeli wprowadzona temperature jest mniejsza niż 0, podnieś `NegativeKelvin`
+    4. Wymagania niefunkcjonalne
+        a. Rozwiązanie zapisz wewnątrz funkcji `check`
+        b. Zwróć uwagę na poziom wcięć
+    5. Porównaj wyniki z sekcją "Tests" (patrz poniżej)
 
 Tests:
     >>> type(temperature)
