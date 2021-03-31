6c22fe4a3e39fe42cb227b4db8b3675293a12d38
Matt Harasymczuk <matt@astrotech.io>
2021-03-31 08:48:01 +0200

    Basics: Mapping #time 10m

--- basics/mapping/assignments/mapping_dict_b.py
+++ basics/mapping/assignments/mapping_dict_b.py
@@ -8,14 +8,14 @@ English:
     1. Use data from "Given" section (see below)
     2. Define `keys: list[str]` with list of `DATA` keys
     3. Define `values: list[str]` with list of `DATA` values
-    4. Define `keys: list[tuple]` with list of `DATA` key-value pairs
+    4. Define `items: list[tuple]` with list of `DATA` key-value pairs
     5. Compare result with "Tests" section (see below)
 
 Polish:
     1. Użyj danych z sekcji "Given" (patrz poniżej)
     2. Zdefiniuj `keys: list[str]` z listą kluczy z `DATA`
     3. Zdefiniuj `values: list[str]` z listą wartości z `DATA`
-    4. Zdefiniuj `keys: list[tuple]` z listą par klucz-wartość z `DATA`
+    4. Zdefiniuj `items: list[tuple]` z listą par klucz-wartość z `DATA`
     5. Porównaj wyniki z sekcją "Tests" (patrz poniżej)
 
 Tests:
