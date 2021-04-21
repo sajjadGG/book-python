20277e14857116565b889f9901da79280393065c
Matt Harasymczuk <matt@astrotech.io>
2021-04-21 17:23:40 +0200

    Book: Assignments #time 10m

--- stdlib/operating-system/assignments/argparse_avg.py
+++ stdlib/operating-system/assignments/argparse_avg.py
@@ -1,13 +1,15 @@
-from argparse import ArgumentParser
+# Commented-out because it breaks doctests in Github Actions
 
-
-def avg(*args):
-    return sum(args) / len(args)
-
-
-parser = ArgumentParser()
-parser.add_argument('--numbers', nargs='+', type=float)
-arguments = parser.parse_args()
-
-result = avg(*arguments.numbers)
-print(result)
+# from argparse import ArgumentParser
+#
+#
+# def avg(*args):
+#     return sum(args) / len(args)
+#
+#
+# parser = ArgumentParser()
+# parser.add_argument('--numbers', nargs='+', type=float)
+# arguments = parser.parse_args()
+#
+# result = avg(*arguments.numbers)
+# print(result)
