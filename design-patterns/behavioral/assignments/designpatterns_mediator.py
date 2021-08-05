5cb25de074f2052768a0aa060ac10afd8c1b262b
Matt Harasymczuk <matt@astrotech.io>
2021-08-05 11:49:15 +0200

    Design Patterns: Mediator #time 20m

--- design-patterns/behavioral/assignments/designpatterns_mediator.py
+++ design-patterns/behavioral/assignments/designpatterns_mediator.py
@@ -1,7 +1,7 @@
 """
 * Assignment: DesignPatterns Behavioral Mediator
 * Complexity: easy
-* Lines of code: 34 lines
+* Lines of code: 15 lines
 * Time: 21 min
 
 English:
@@ -30,57 +30,70 @@ Tests:
     >>> form.submit()
     'Submitted'
 """
+from __future__ import annotations
 from abc import ABCMeta, abstractmethod
-from dataclasses import dataclass, field
-
-
-class EventHandler(metaclass=ABCMeta):
-    @abstractmethod
-    def __call__(self) -> None:
-        pass
+from dataclasses import dataclass
+from typing import Any
 
 
 @dataclass
-class UIControl(metaclass=ABCMeta):
-    name: str
-    __observers: list[EventHandler] = field(default_factory=list)
+class UIElement(metaclass=ABCMeta):
+    _name: str
+    _owner: Form
+    _value: Any
 
-    def add_event_handler(self, observer: EventHandler) -> None:
-        self.__observers.append(observer)
-
-    def _notify_event_handlers(self):
-        for observer in self.__observers:
-            observer.__call__()
-
-
-
-class Input(UIControl):
-    __content: str
-
-    def get_content(self) -> str:
+    def changed(self):
         raise NotImplementedError
 
-    def set_content(self, content: str) -> None:
+    @abstractmethod
+    def set_value(self, value: Any) -> None: ...
+
+    @abstractmethod
+    def get_value(self) -> Any: ...
+
+
+@dataclass
+class Input(UIElement):
+    _value: str = ''
+
+    def get_value(self) -> str:
+        raise NotImplementedError
+
+    def set_value(self, value: str) -> None:
         raise NotImplementedError
 
 
-class Button(UIControl):
-    __enabled: bool
+@dataclass
+class Button(UIElement):
+    _value: bool = False
 
-    def set_enabled(self, enabled: bool) -> None:
+    def set_value(self, value: bool) -> None:
         raise NotImplementedError
+
+    def get_value(self) -> Any:
+        raise NotImplementedError
+
+    def enable(self):
+        self.set_value(True)
+
+    def disable(self):
+        self.set_value(False)
 
     def is_enabled(self) -> bool:
-        raise NotImplementedError
+        return self._value
 
 
-@dataclass
-class LoginForm:
-    __username_input: Input = Input('Username')
-    __password_input: Input = Input('Password')
-    __submit_button: Button = Button('Submit')
+class Form(metaclass=ABCMeta):
+    @abstractmethod
+    def on_change(self): ...
 
-    def __post_init__(self):
+
+class LoginForm(Form):
+    username_input: Input
+    password_input: Input
+    submit_button: Button
+
+    def __init__(self):
         raise NotImplementedError
 
     def set_username(self, username: str):
@@ -89,98 +102,94 @@ class LoginForm:
     def set_password(self, password: str):
         raise NotImplementedError
 
-    def __username_changed(self) -> None:
-        raise NotImplementedError
-
-    def __password_changed(self) -> None:
-        raise NotImplementedError
-
-    def __try_enable_submit(self):
+    def on_change(self):
         raise NotImplementedError
 
     def submit(self):
-        raise NotImplementedError
+        if self.submit_button.is_enabled():
+            return 'Submitted'
+        else:
+            raise PermissionError('Cannot submit form without Username and Password')
 
 
 # Solution
-from abc import ABCMeta, abstractmethod
-from dataclasses import dataclass, field
+@dataclass
+class UIElement(metaclass=ABCMeta):
+    _name: str
+    _owner: Form
+    _value: Any
 
+    def changed(self):
+        self._owner.on_change()
 
-class EventHandler(metaclass=ABCMeta):
     @abstractmethod
-    def __call__(self) -> None:
-        pass
+    def set_value(self, value: Any) -> None: ...
+
+    @abstractmethod
+    def get_value(self) -> Any: ...
 
 
 @dataclass
-class UIControl(metaclass=ABCMeta):
-    name: str
-    __observers: list[EventHandler] = field(default_factory=list)
+class Input(UIElement):
+    _value: str = ''
 
-    def add_event_handler(self, observer: EventHandler) -> None:
-        self.__observers.append(observer)
+    def get_value(self) -> str:
+        return self._value
 
-    def _notify_event_handlers(self):
-        for observer in self.__observers:
-            observer.__call__()
-
-
-class Input(UIControl):
-    __content: str
-
-    def get_content(self) -> str:
-        if hasattr(self, '__content'):
-            return self.__content
-
-    def set_content(self, content: str) -> None:
-        self.__content = content
-        self._notify_event_handlers()
+    def set_value(self, value: str) -> None:
+        self._value = value
+        self.changed()
 
 
-class Button(UIControl):
-    __enabled: bool
+@dataclass
+class Button(UIElement):
+    _value: bool = False
 
-    def set_enabled(self, enabled: bool) -> None:
-        self.__enabled = enabled
-        self._notify_event_handlers()
+    def set_value(self, value: bool) -> None:
+        self._value = value
+
+    def get_value(self) -> Any:
+        return self._value
+
+    def enable(self):
+        self.set_value(True)
+
+    def disable(self):
+        self.set_value(False)
 
     def is_enabled(self) -> bool:
-        if not hasattr(self, '__enabled'):
-            return False
-        return self.__enabled
+        return self._value
 
 
-@dataclass
-class LoginForm:
-    __username_input: Input = Input('Username')
-    __password_input: Input = Input('Password')
-    __submit_button: Button = Button('Submit')
+class Form(metaclass=ABCMeta):
+    @abstractmethod
+    def on_change(self): ...
 
-    def __post_init__(self):
-        self.__username_input.add_event_handler(self.__username_changed)
-        self.__password_input.add_event_handler(self.__password_changed)
+
+class LoginForm(Form):
+    username_input: Input
+    password_input: Input
+    submit_button: Button
+
+    def __init__(self):
+        self.username_input = Input('Username', self)
+        self.password_input = Input('Password', self)
+        self.submit_button = Button('Submit', self)
 
     def set_username(self, username: str):
-        self.__username_input.set_content(username)
+        self.username_input.set_value(username)
 
     def set_password(self, password: str):
-        self.__password_input.set_content(password)
+        self.password_input.set_value(password)
 
-    def __username_changed(self) -> None:
-        self.__try_enable_submit()
-
-    def __password_changed(self) -> None:
-        self.__try_enable_submit()
-
-    def __try_enable_submit(self):
-        username = self.__username_input.get_content()
-        password = self.__password_input.get_content()
-        if username is not None and password is not None:
-            self.__submit_button.set_enabled(True)
+    def on_change(self):
+        username = self.username_input.get_value()
+        password = self.password_input.get_value()
+        if username and password:
+            self.submit_button.enable()
 
     def submit(self):
-        if not self.__submit_button.is_enabled():
-            raise PermissionError('Cannot submit form without Username and Password')
-        else:
+        if self.submit_button.is_enabled():
             return 'Submitted'
+        else:
+            raise PermissionError('Cannot submit form without Username and Password')
