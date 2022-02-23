0dd29b1be335158c8d2093b88f291232fbdc871b
Matt Harasymczuk <matt@astrotech.io>
2022-02-23 11:41:24 +0100

    Database: SQLAlchemy #time 10m

--- intermediate/about/assignments/about_entrytest_d.py
+++ intermediate/about/assignments/about_entrytest_d.py
@@ -1,29 +1,36 @@
 """
-* Assignment: About EntryTest File
+* Assignment: About EntryTest ListDict
 * Complexity: hard
-* Lines of code: 16 lines
+* Lines of code: 15 lines
 * Time: 13 min
 
 English:
-    1. Skip comments (`#`) and empty lines
-    2. Extract from each line: ip, host and protocol and add to `result: list[dict]`
-    3. Each line must be a separate dict
-    4. Merge host names with the same IP
-    5. IPv4 protocol address is when dot (`.`) is in ip address
-    6. `result` must be `list[dict]`
+    1. Define `result: list[dict]`, where each dict has keys:
+       * address: str
+       * hosts: list[str]
+    2. Iterate over lines in `DATA` skipping comments (`#`) and empty lines
+    3. Extract from each line: `address` and `hosts`
+    4. Add `address` and `hosts` to `result` as a dict, example:
+       {'address': '127.0.0.1', 'hosts': ['localhost', 'astromatt']}
+    5. Each line must be a separate dict
+    6. Merge host names with the same IP (important!)
     7. Run doctests - all must succeed
 
 Polish:
-    1. Pomiń komentarze (`#`) i puste linie
-    2. Wyciągnij z każdej linii: ip, host i protokół i dodaj do `result: list[dict]`
-    3. Każda linia ma być osobnym słownikiem
-    4. Scal nazwy hostów dla tego samego IP
-    5. Protokół IPv4 jest gdy kropka (`.`) znajduje się w adresie
-    6. `result` musi być `list[dict]`
+    1. Zdefiniuj `result: list[dict]`, gdzie każdy dict ma klucze:
+       * address: str
+       * hosts: list[str]
+    2. Iteruje po liniach w `DATA` pomijając komentarze (`#`) i puste linie
+    3. Wyciągnij z każdej linii: `address` i `hosts`
+    4. Dodaj `address` i `hosts` do `result` jako słownik, przykład:
+       {'address': '127.0.0.1', 'hosts': ['localhost', 'astromatt']}
+    5. Każda linia ma być osobnym słownikiem
+    6. Scal nazwy hostów dla tego samego IP (ważne)
     7. Uruchom doctesty - wszystkie muszą się powieść
 
 Tests:
     >>> import sys; sys.tracebacklimit = 0
+    >>> from pprint import pprint
 
     >>> assert result is not Ellipsis, \
     'Assign result to variable: `result` instead of Ellipsis `...`'
@@ -33,18 +40,17 @@ Tests:
     'Variable `result` has invalid type, should be list'
     >>> assert all(type(x) is dict for x in result), \
     'All keys in `result` should be dict'
-    >>> assert [x['ip'] for x in result].count('127.0.0.1') == 1, \
+    >>> assert [x['address'] for x in result].count('127.0.0.1') == 1, \
     'You did not merge hostnames for the same ip (127.0.0.1)'
 
-    >>> result  # doctest: +NORMALIZE_WHITESPACE
-    [{'ip': '127.0.0.1', 'hosts': ['localhost', 'astromatt'], 'protocol': 'ipv4'},
-     {'ip': '10.13.37.1', 'hosts': ['nasa.gov', 'esa.int', 'roscosmos.ru'], 'protocol': 'ipv4'},
-     {'ip': '255.255.255.255', 'hosts': ['broadcasthost'], 'protocol': 'ipv4'},
-     {'ip': '::1', 'hosts': ['localhost'], 'protocol': 'ipv6'}]
+    >>> pprint(result, width=120)
+    [{'address': '127.0.0.1', 'hosts': ['localhost', 'astromatt']},
+     {'address': '10.13.37.1', 'hosts': ['nasa.gov', 'esa.int', 'roscosmos.ru']},
+     {'address': '255.255.255.255', 'hosts': ['broadcasthost']},
+     {'address': '::1', 'hosts': ['localhost']}]
 """
 
-DATA = """
-##
+DATA = """##
 # `/etc/hosts` structure:
 #   - IPv4 or IPv6
 #   - Hostnames
@@ -54,11 +60,10 @@ DATA = """
 127.0.0.1       astromatt
 10.13.37.1      nasa.gov esa.int roscosmos.ru
 255.255.255.255 broadcasthost
-::1             localhost
-"""
+::1             localhost"""
 
-# list[dict]: keys: ip, hosts, protocol; merge hosts for the same ip address
-#             protocol is "ipv4" when '.' is in address; use conditional expr.
+# list[dict]: keys: address, hosts, protocol; merge hosts for the same ip
+#             protocol is 'ipv4' when '.' is in address; use conditional expr.
 result = ...
 
 
@@ -72,15 +77,12 @@ for line in DATA.splitlines():
     elif line.startswith('#'):
         continue
 
-    ip, *hosts = line.split()
+    address, *hosts = line.split()
 
     for row in result:
-        if row['ip'] == ip:
+        if row['address'] == address:
             row['hosts'] += hosts
             break
     else:
-        result.append({
-            'ip': ip,
-            'hosts': hosts,
-            'protocol': 'ipv4' if '.' in ip else 'ipv6'
-        })
+        row = {'address': address, 'hosts': hosts}
+        result.append(row)
