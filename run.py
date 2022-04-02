ff4a34ace0aa743a49b1aeeb8056ac04bb52cedc
Matt Harasymczuk <matt@astrotech.io>
2022-04-02 15:41:17 +0200

    Book: Tests #time 14m

--- run.py
+++ run.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python
+"""
+usage: run.py [-h] [-b [CHAPTER]] [-t [CHAPTER]] [-i [CHAPTER]] [-g]
+
+options:
+  -h, --help            show this help message and exit
+  -b [CHAPTER], --build [CHAPTER]
+                        build documentation in html format
+  -t [CHAPTER], --doctest [CHAPTER]
+                        test ReST files
+  -i [CHAPTER], --install [CHAPTER]
+                        install requirements
+  -g, --gather          gather chapter requirements to main requirements
+
+example:
+    run.py --install
+    run.py --install basics
+    run.py --doctest
+    run.py --doctest basics
+    run.py --doctest basics/types
+"""
+
 import logging
 import sys
 from pathlib import Path
@@ -11,7 +32,6 @@ logging.basicConfig(level='INFO', format='%(message)s')
 log = logging.getLogger(__name__)
 
 BASE_DIR = Path(__file__).parent
-
 
 
 def run(cmd, timeout=None):
@@ -45,7 +65,6 @@ class RequirementsInstall(Action):
 
 class Build(Action):
     def __call__(self, parser, namespace, chapter, *args, **kwargs):
-        run('clear && printf "\e[3J"')  # noqa
         if chapter is None:
             src = BASE_DIR
             dst = Path('/tmp') / 'pybook'
@@ -53,11 +72,27 @@ class Build(Action):
             src = BASE_DIR / chapter
             dst = Path('/tmp') / chapter
         rmtree(dst, ignore_errors=True)
+        run('clear && printf "\e[3J"')  # noqa
         run(f'sphinx-build -a -E -j auto --color -b html {src} {dst}')
 
 
 class TestDoctest(Action):
-    def is_ignored(self, file: Path):
+    def __call__(self, parser, namespace, chapter, *args, **kwargs):
+        run('clear && printf "\e[3J"')  # noqa
+        all_tests = 0
+        for file in sorted(self.get_files(chapter)):
+            if self.is_ignored(file): continue
+            if self.is_venv(file): continue
+            if self.is_skipped(file): continue
+            if count := self.count_doctests(file):
+                self.run_doctest(file)
+                all_tests += count
+            else:
+                log.error(f'NOTESTS\t{file}')
+        logging.warning(f'\nAll tests {all_tests}')
+
+    @staticmethod
+    def is_ignored(file: Path):
         doctestignore = Path(file.parts[0]) / '.doctestignore'
         if doctestignore.exists():
             log.warning(f'IGNORED\t{file}')
@@ -65,27 +100,32 @@ class TestDoctest(Action):
         else:
             return False
 
-    def is_skipped(self, file: Path):
+    @staticmethod
+    def is_skipped(file: Path):
         if '# doctest: +SKIP_FILE' in file.read_text():
             log.warning(f'SKIPPED\t{file}')
             return True
         else:
             return False
 
-    def is_venv(self, file: Path):
+    @staticmethod
+    def is_venv(file: Path):
         log.debug(f'VENV\t{file}')
         return str(file).startswith('.venv-py')
 
-    def count_doctests(self, file: Path):
+    @staticmethod
+    def count_doctests(file: Path):
         return file.read_text().count('>>>')
 
-    def get_files(self, directory: str):
+    @staticmethod
+    def get_files(directory: str):
         if directory is None:
             directory = ''
         yield from Path(directory).rglob('*.rst')
         yield from Path(directory).rglob('*.py')
 
-    def run_test(self, file: Path):
+    @staticmethod
+    def run_doctest(file: Path):
         log.debug(f'RUN\t {file}')
         exitcode = run(f'python -m doctest {file}', timeout=20)
         if exitcode == 0:
@@ -94,39 +134,25 @@ class TestDoctest(Action):
             log.critical(f'FAILED\t{file}')
             exit(1)
 
-    def __call__(self, parser, namespace, chapter, *args, **kwargs):
-        run('clear && printf "\e[3J"')  # noqa
-        all_tests = 0
-        for file in sorted(self.get_files(chapter)):
-            if self.is_ignored(file): continue
-            if self.is_venv(file): continue
-            if self.is_skipped(file): continue
-            if ntests := self.count_doctests(file):
-                self.run_test(file)
-                all_tests += ntests
-            else:
-                log.error(f'NOTESTS\t{file}')
-        logging.warning(f'\nAll tests {all_tests}')
-
 
 if __name__ == '__main__':
     parser = ArgumentParser()
 
     parser.add_argument('-b', '--build',
                         nargs='?', metavar='CHAPTER', action=Build,
-                        help='Build documentation in html format')
+                        help='build documentation in html format')
 
     parser.add_argument('-t', '--doctest',
                         nargs='?', metavar='CHAPTER', action=TestDoctest,
-                        help='Test ReST files')
+                        help='test ReST files')
 
     parser.add_argument('-i', '--install',
                         nargs='?', metavar='CHAPTER', action=RequirementsInstall,
-                        help='Install requirements')
+                        help='install requirements')
 
-    parser.add_argument('-r', '--gather',
+    parser.add_argument('-g', '--gather',
                         nargs=0, action=RequirementsGather,
-                        help='Gather chapter requirements to main requirements')
+                        help='gather chapter requirements to main requirements')
 
     parser.parse_args()
 
